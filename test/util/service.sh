b7a6798f1da43a7216513c496a41794879951137
Braxton Huggins <braxton@pagodabox.com>
2018-06-15 16:26:29 -0600

    add configure, start, stop, export-final, export-live, import-clean, import hooks. Update test payloads, remove unneeded test

--- test/util/service.sh
+++ test/util/service.sh
@@ -1,28 +1,20 @@
 
-service_name="Redis"
-default_port=6379
+service_name="Factory"
+default_port=7419
 
 wait_for_running() {
   container=$1
-  until docker exec ${container} bash -c "ps aux | grep [r]edis-server | grep -v sentinel | grep -v 26379"
+  until docker exec ${container} bash -c "ps aux | grep [f]aktory"
   do
     sleep 1
   done
 }
 
-wait_for_arbitrator_running() {
-  container=$1
-  until docker exec ${container} bash -c "ps aux | grep [r]edis-server | grep sentinel"
-  do
-    sleep 1
-  done
-}
 
 wait_for_listening() {
   container=$1
   ip=$2
   port=$3
-  run docker exec ${container} bash -c "if [ -f /etc/service/proxy/run ]; then sv restart proxy; fi;"
   until docker exec ${container} bash -c "nc -q 1 ${ip} ${port} < /dev/null"
   do
     sleep 1
@@ -31,7 +23,7 @@ wait_for_listening() {
 
 wait_for_stop() {
   container=$1
-  while docker exec ${container} bash -c "ps aux | grep [r]edis-server | grep -v sentinel | grep -v 26379"
+  while docker exec ${container} bash -c "ps aux | grep [f]aktory"
   do
     sleep 1
   done
@@ -39,7 +31,7 @@ wait_for_stop() {
 
 verify_stopped() {
   container=$1
-  run docker exec ${container} bash -c "ps aux | grep [r]edis-server | grep -v sentinel | grep -v 26379"
+  run docker exec ${container} bash -c "ps aux | grep [f]aktory"
   echo_lines
   [ "$status" -eq 1 ] 
 }
@@ -50,9 +42,7 @@ insert_test_data() {
   port=$3
   key=$4
   data=$5
-  run docker exec ${container} bash -c "if [ -f /etc/service/proxy/run ]; then sv restart proxy; fi;"
-  sleep 5
-  run docker exec ${container} bash -c "/data/bin/redis-cli set ${key} ${data}"
+  # run docker exec ${container} bash -c "some command"
   [ "$status" -eq 0 ]
 }
 
@@ -62,9 +52,7 @@ update_test_data() {
   port=$3
   key=$4
   data=$5
-  run docker exec ${container} bash -c "if [ -f /etc/service/proxy/run ]; then sv restart proxy; fi;"
-  sleep 5
-  run docker exec ${container} bash -c "/data/bin/redis-cli set ${key} ${data}"
+  # run docker exec ${container} bash -c "some command"
   [ "$status" -eq 0 ]
 }
 
@@ -74,9 +62,7 @@ verify_test_data() {
   port=$3
   key=$4
   data=$5
-  run docker exec ${container} bash -c "if [ -f /etc/service/proxy/run ]; then sv restart proxy; fi;"
-  sleep 5
-  run docker exec ${container} bash -c "/data/bin/redis-cli get ${key}"
+  # run docker exec ${container} bash -c "some command"
   echo_lines
   [ "${lines[0]}" = "${data}" ]
   [ "$status" -eq 0 ]
@@ -86,15 +72,13 @@ verify_plan() {
   [ "${lines[0]}" = "{" ]
   [ "${lines[1]}" = "  \"redundant\": false," ]
   [ "${lines[2]}" = "  \"horizontal\": false," ]
-  [ "${lines[3]}" = "  \"users\": [" ]
-  [ "${lines[4]}" = "  ]," ]
-  [ "${lines[5]}" = "  \"ips\": [" ]
-  [ "${lines[6]}" = "    \"default\"" ]
-  [ "${lines[7]}" = "  ]," ]
-  [ "${lines[8]}" = "  \"port\": 6379," ]
-  [ "${lines[9]}" = "  \"behaviors\": [" ]
-  [ "${lines[10]}" = "    \"migratable\"," ]
-  [ "${lines[11]}" = "    \"backupable\"" ]
-  [ "${lines[12]}" = "  ]" ]
-  [ "${lines[13]}" = "}" ]
+  [ "${lines[3]}" = "  \"user\": \"nanobox\"," ]
+  [ "${lines[4]}" = "  \"users\": [{" ]
+  [ "${lines[5]}" = "    \"username\": \"nanobox\"," ]
+  [ "${lines[6]}" = "    \"meta\": {}" ]
+  [ "${lines[7]}" = "  }]," ]
+  [ "${lines[8]}" = "  \"ips\": [\"default\"]," ]
+  [ "${lines[9]}" = "  \"port\": 7419," ]
+  [ "${lines[10]}" = "  \"behaviors\": [\"migratable\"]" ]
+  [ "${lines[11]}" = "}" ]
 }
